41ca7eeb2932cdfba6c8fd9c9cf7271dc88bd1a1
Tammy Metz <tammy@tammymetz.com>
2018-09-15 23:27:34 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,15 @@
 module Findable
   module ClassMethods
+
+    def self.find_or_create_by_name(name)
+      found_object = self.find_by_name(name)
+      if found_object
+        found_object
+      else
+        self.create(name)
+      end
+    end
+    
     def find_by_name(name)
       self.all.detect {|a| a.name == name}
     end
