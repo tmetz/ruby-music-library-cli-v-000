ac09cf87e5aa3b9415fffb82823c0f288d13da20
Tammy Metz <tammy@tammymetz.com>
2018-09-15 20:40:11 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+class Genre
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    genre = Genre.new(name)
+    @@all << genre
+    genre
+  end
+end
