a6d4b2ff342148a575b8142a87f4afa3f715ce8e
Tammy Metz <tammy@tammymetz.com>
2018-09-15 20:36:23 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+
+  @@all = []
+  
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    @@all << song
+    song
+end
