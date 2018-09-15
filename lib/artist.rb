2ee0680884b1ea7f77e1e7a05cd396f812666986
Tammy Metz <tammy@tammymetz.com>
2018-09-15 20:57:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,9 +3,11 @@ class Artist
   @@all = []
 
   attr_accessor :name
+  attr_writer :songs
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def self.all
@@ -27,6 +29,11 @@ class Artist
   end
 
   def add_song(song)
-    song.artist = self
+    if !exists?(song.artist)
+      song.artist = self
+    end
+    if !@songs.include?(song)
+      @songs << song
+    end
   end
 end
