9363778eed762e280ff38c44f59661dce2851527
Tammy Metz <tammy@tammymetz.com>
2018-09-16 01:07:51 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -63,8 +63,9 @@ class MusicLibraryController
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     artist_name = gets.strip
-    name_array = Artist.find_by_name(artist_name).sort_by {|song| song.name}
-    name_array.each_with_index do |song, index|
+    artist = Artist.find_by_name(artist_name)
+    songs_array = artist.songs.sort_by {|song| song.name}
+    songs_array.each_with_index do |song, index|
       real_index = index + 1
       puts "#{real_index}. #{song.name} - #{song.genre}"
     end
